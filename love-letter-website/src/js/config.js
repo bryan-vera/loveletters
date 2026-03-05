996a76cb563a769d74aaf61caaf0611899245c9f
Bryan Vera <bvera@bryanvera.com>
2026-03-05 16:16:53 -0500

    add config

---
+++ love-letter-website/src/js/config.js
@@ -0,0 +1,5 @@
+// Supabase configuration - keep this file out of version control
+window.SUPABASE_CONFIG = {
+    url: 'https://qvttnfblzpcecakxuxxj.supabase.co',
+    anonKey: 'sb_publishable_Fw9BNBXh_5KABPmTORdtmQ_kYlM4kk1'
+};
